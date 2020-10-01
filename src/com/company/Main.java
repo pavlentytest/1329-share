951033f94dcc15e710de6e9ca260694469cd5ce4
User <aaredov@bk.ru>
2020-10-01 15:19:25 -0200

    убрали все

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,19 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    Scanner scan = new Scanner(System.in);
-	    int x = scan.nextInt();
-	    // добавил комментарий
-	    if(x >= 100) {
-	    // МОй КОММЕНТ
-        // Я запрещаю запрещать мне писать нецензурные выражения
-        // Идите вы все в ПТУ...
-	    if(x > 100) {
-	        System.out.println("x>100");
-        } else {
-            System.out.println("x<100");
-        }
-	    // тут был Юра
-    }
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
 
+        }
 }
+
+
