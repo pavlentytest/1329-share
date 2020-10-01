05329a7001952630bf5a5007c669203981b5a181
Yury Yartsev <yury-yartsev@yandex.ru>
2020-10-01 20:14:13 +0300

    Hello there

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,7 @@ public class Main {
 	    int x = scan.nextInt();
 	    // МОй КОММЕНТ
         // Я запрещаю запрещать мне писать нецензурные выражения
+        // Идите вы все
 	    if(x > 100) {
 	        System.out.println("x>100");
         } else {//это Олег
